03f25722dcce30c65554fd504ce3de8b976dd3b5
RinShishio <linggarichsan@gmail.com>
2021-02-17 12:59:33 +0700

    Create LoopWhile.js

---
+++ Tugas-3-Loop/LoopWhile.js
@@ -0,0 +1,14 @@
+var flag = 1;
+console.log("Looping Pertama")
+
+while(flag <= 20) { 
+  console.log(flag+" - I love coding"); 
+  flag++;
+}
+
+var flag1 = 20;
+console.log("Looping Kedua")
+while(flag1 >= 1 ){ 
+  console.log(flag1+" - I love coding"); 
+  flag1--;
+}
