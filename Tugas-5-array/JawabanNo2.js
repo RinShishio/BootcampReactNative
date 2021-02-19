fe7c4f2867164af5222ad604d9b04f90770f7faa
RinShishio <linggarichsan@gmail.com>
2021-02-20 00:17:03 +0700

    Create JawabanNo2.js

---
+++ Tugas-5-array/JawabanNo2.js
@@ -0,0 +1,17 @@
+function rangeWithStep(min, max, step){
+    let arr = [];
+    if (min < max) {
+        for(let i = min; i <= max; i += step){
+            arr.push(i);
+         }
+    }else if (min>max) {
+        for(let i = min; i >= max; i -= step){
+            arr.push(i);
+         }
+    }
+    return arr;
+  }
+  console.log(rangeWithStep(1, 10, 2)) // [1, 3, 5, 7, 9]
+  console.log(rangeWithStep(11, 23, 3)) // [11, 14, 17, 20, 23]
+  console.log(rangeWithStep(5, 2, 1)) // [5, 4, 3, 2]
+  console.log(rangeWithStep(29, 2, 4)) // [29, 25, 21, 17, 13, 9, 5] 
