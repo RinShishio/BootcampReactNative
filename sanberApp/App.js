636abcfe60d2f7f64530ed5b4d5685472d715a2e
RinShishio <linggarichsan@gmail.com>
2021-03-02 19:15:49 +0700

    Update App.js

--- sanberApp/App.js
+++ sanberApp/App.js
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Telegram from './Tugas/Tugas12/Telegram';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <Telegram />
   );
 }
 
@@ -16,6 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
